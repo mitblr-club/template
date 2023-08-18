16c57bf4716c8a6de6d58e1d7c1eb1c518459a97
Abhigyan Tripathi <abhigyantrips@gmail.com>
2023-08-19 00:55:50 +0530

    feat: init tailwind, shadcn components

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,16 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			runtime: 'nodejs18.x',
-		})
+			runtime: 'nodejs18.x'
+		}),
+		alias: {
+			$components: 'src/lib/components',
+			'$components/*': 'src/lib/components/*'
+		}
 	}
 };
-
 export default config;
