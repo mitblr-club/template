0da928c7929a00aa68ac6560350e3a8167474341
Abhigyan Tripathi <abhigyantrips@gmail.com>
2023-10-30 23:33:07 +0530

    chore: init nextjs template

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<div class="absolute left-[50%] top-[50%] m-0 translate-x-[-50%] translate-y-[-50%]">
-	<h1 class="block h-full p-4 text-center text-6xl font-bold">Home Page</h1>
-</div>
