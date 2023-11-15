86a5fae2afec71cd48a4f1652d03e8cbf20fec8b
Abhigyan Tripathi <abhigyantrips@gmail.com>
2023-11-15 23:56:42 +0530

    feat: update icons and imports

--- components/icon-list.tsx
+++ components/icon-list.tsx
@@ -1,56 +1,74 @@
+import { siteConfig } from '@/site.config';
+
+import Link from 'next/link';
+
 import { Icons } from '@/components/icons';
 import { buttonVariants } from '@/components/ui/button';
-import { siteConfig } from '@/config/site';
-import Link from 'next/link';
 
 const IconList = () => {
   return (
-    <nav className='flex items-center space-x-1'>
-      <Link href={siteConfig.links.instagram} target='_blank' rel='noreferrer'>
+    <nav className="flex items-center space-x-1">
+      <Link
+        href={siteConfig.mediaLinks.instagram}
+        target="_blank"
+        rel="noreferrer"
+      >
         <div
           className={buttonVariants({
             size: 'icon',
             variant: 'ghost',
           })}
         >
-          <Icons.instagram className='h-5 w-5' />
-          <span className='sr-only'>Instagram</span>
+          <Icons.instagram className="h-5 w-5" />
+          <span className="sr-only">Instagram</span>
         </div>
       </Link>
 
-      <Link href={siteConfig.links.twitter} target='_blank' rel='noreferrer'>
+      <Link
+        href={siteConfig.mediaLinks.twitter}
+        target="_blank"
+        rel="noreferrer"
+      >
         <div
           className={buttonVariants({
             size: 'icon',
             variant: 'ghost',
           })}
         >
-          <Icons.twitter className='h-5 w-5' />
-          <span className='sr-only'>Twitter</span>
+          <Icons.twitter className="h-5 w-5" />
+          <span className="sr-only">Twitter</span>
         </div>
       </Link>
 
-      <Link href={siteConfig.links.linkedin} target='_blank' rel='noreferrer'>
+      <Link
+        href={siteConfig.mediaLinks.linkedin}
+        target="_blank"
+        rel="noreferrer"
+      >
         <div
           className={buttonVariants({
             size: 'icon',
             variant: 'ghost',
           })}
         >
-          <Icons.linkedin className='h-5 w-5 fill-current' />
-          <span className='sr-only'>LinkedIn</span>
+          <Icons.linkedin className="h-5 w-5 fill-current" />
+          <span className="sr-only">LinkedIn</span>
         </div>
       </Link>
 
-      <Link href={siteConfig.links.github} target='_blank' rel='noreferrer'>
+      <Link
+        href={siteConfig.mediaLinks.github}
+        target="_blank"
+        rel="noreferrer"
+      >
         <div
           className={buttonVariants({
             size: 'icon',
             variant: 'ghost',
           })}
         >
-          <Icons.github className='h-5 w-5' />
-          <span className='sr-only'>GitHub</span>
+          <Icons.github className="h-5 w-5" />
+          <span className="sr-only">GitHub</span>
         </div>
       </Link>
     </nav>
