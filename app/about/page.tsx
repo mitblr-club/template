42694e51ead91ad39128bb55aa3b1f5b2aaf8e4e
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2023-12-31 16:09:53 +0530

    feat: add base design of about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,15 @@
+import { How, What, Why } from './questionComponents/data';
+import Quest from './questionComponents/quest';
+
 const About = () => {
   return (
-    <div className="absolute left-[50%] top-[50%] m-0 translate-x-[-50%] translate-y-[-50%]">
-      <h1 className="block h-full p-4 text-center text-6xl font-bold">
-        About Page
-      </h1>
+    <div className="mt-c20 px-0 pb-c3 md:mt-c10 xl:mt-c5 xl:px-c8">
+      <h1 className="text-center text-6xl font-bold">About Us</h1>
+      <div className="mt-c20 flex flex-col gap-12 p-c6 md:mt-c10 lg:mt-c5 xl:mt-c1">
+        <Quest header={'What'} content={What} />
+        <Quest header={'Why'} content={Why} />
+        <Quest header={'How'} content={How} />
+      </div>
     </div>
   );
 };
---
+++ app/about/questionComponents/data.ts
@@ -0,0 +1,20 @@
+export const What = [
+  'This is card one',
+  'This is card two',
+  'This is card three plus some extra content',
+  'This is card four and some other stuff just to make it longer cuz I wanna see the sizes of the cards',
+];
+
+export const Why = [
+  'This is card one',
+  'This is card two',
+  'This is card three plus some extra content',
+  'This is card four and some other stuff just to make it longer cuz I wanna see the sizes of the cards',
+];
+
+export const How = [
+  'This is card one',
+  'This is card two',
+  'This is card three plus some extra content',
+  'This is card four and some other stuff just to make it longer cuz I wanna see the sizes of the cards',
+];
