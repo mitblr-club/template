b882d4f1ffcc91e5c4751af9f5d8a3228498455d
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-06-08 15:48:46 +0530

    feat(assets): update logo, dummy brand

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,7 @@ export const metadata: Metadata = {
     { media: '(prefers-color-scheme: dark)', color: 'black' },
   ],
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
+    icon: '/favicon.png',
   },
 };
 
